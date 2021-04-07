9b0c977da507d5bd43963151a7f168c8a275f5c6
Danish ranjan <ranjandanish@gmail.com>
2021-04-07 22:36:43 +0530

    Exit Point Of A Matrix

---
+++ ExitPointOfAMatrix.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class ExitPointOfAMatrix {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[n][m];
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        
+        int i=0;
+        int j =0;
+        int dir = 0;
+        //0 stands for East 
+        //1 stands for South
+        //2 stands for west
+        //3 stands for north
+        
+        while(true){
+            dir = (dir + arr[i][j]) % 4;
+            if(dir == 0){
+                j++;
+            }else if(dir == 1){
+                i++;
+            }else if(dir == 2){
+                j--;
+            }else if(dir == 3){
+                i--;
+            }
+            
+            if(i < 0){
+                i++;
+                break;
+            }else if(j < 0){
+                j++;
+                break;
+            }else if( i == arr.length){
+                i--;
+                break;
+            }else if( j == arr[0].length){
+                j--;
+                break;
+            }
+        }
+        System.out.println(i);
+        System.out.println(j);
+    }
+
+}
