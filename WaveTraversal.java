4f12b800f16895037ebbd4f954597b7a258a35cf
Danish ranjan <ranjandanish@gmail.com>
2021-04-07 23:57:29 +0530

    Rotate an Array by 90 degree

--- WaveTraversal.java
+++ WaveTraversal.java
@@ -1,4 +1,32 @@
+import java.io.*;
+import java.util.*;
 
-public class WaveTraversal {
+public class WaveTraversal{
+
+public static void main(String[] args) throws Exception {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int m = sc.nextInt();
+    int[][] arr = new int[n][m];
+    for(int i=0;i<arr.length;i++){
+        for(int j=0;j<arr[i].length;j++){
+            arr[i][j] = sc.nextInt();
+        }
+    }
+    
+    for(int j=0;j<arr[0].length;j++){
+        if(j%2==0){
+            for(int i=0;i<arr.length;i++){
+                System.out.println(arr[i][j]);
+            }
+        }
+        else{
+            for(int i=arr.length-1;i>=0;i--){
+                System.out.println(arr[i][j]);
+            }
+        }
+    }
+    
+ }
 
 }
